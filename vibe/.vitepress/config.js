405ff4efbf5ef288d576ac056829f18bf56a050b
dzr975336710 <975336710@qq.com>
2022-02-07 14:28:38 +0800

    BUILD: init project

--- vibe/.vitepress/config.js
+++ vibe/.vitepress/config.js
@@ -18,7 +18,15 @@ module.exports = (async () => {
       ...base.vite,
       plugins: createVitePlugins(),
       build: {
-        minify: false
+        target: 'modules',
+        assetsDir: 'assets', // 指定生成静态资源的存放路径
+        minify: 'terser', // 混淆器，terser构建后文件体积更小
+        terserOptions: {
+          compress: {
+            keep_infinity: true,
+            // drop_console: VITE_DROP_CONSOLE,
+          },
+        },
       },
       resolve: {
         alias: {
