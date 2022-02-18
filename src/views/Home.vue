a1a918efc52185ad6d2719149ad14fc12faa8a2d
rhysonLin <rhyson@vibe.us>
2022-02-18 14:30:23 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,14 +15,12 @@ import HomeFeatures from '/@vcomp/HomeFeatures.vue';
 import HomeFooter from '/@vcomp/HomeFooter.vue';
 </script>
 
-<style scoped>
-.home {
-  padding-top: var(--header-height);
-}
+<style lang="sass" scoped>
+.home
+  padding-top: var(--header-height)
 
-.home-content {
-  max-width: 960px;
-  margin: 0px auto;
-  padding: 0 1.5rem;
-}
+.home-content
+  max-width: 960px
+  margin: 0px auto
+  padding: 0 1.5rem
 </style>
