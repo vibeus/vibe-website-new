044943d46e9baf0d1c4c5672bf79bb59fa42b707
dzr975336710 <975336710@qq.com>
2022-03-16 13:53:47 +0800

    UPDATE: nav

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -1,14 +1,55 @@
 export default [
   {
-    text: 'Solutions',
-    activeMatch: `^/(lp|examples)/`,
+    text: 'Product',
+    // activeMatch: `^/(product)/`,
     items: [
-      { text: 'Education', link: '/lp/scenario-education/' },
-      { text: 'Business', link: '/lp/scenario-business/' }
+      { text: 'Vibe Smartboard 55″', link: '/products/vibe-smartboard-55/' },
+      { text: 'Vibe Smartboard Pro 75″', link: '/products/vibe-smartboard-pro-75/' },
+      { text: 'Vibe Software', link: '/software/' },
+      { text: 'App Integrations', link: '/android-app-store/' },
+      { text: 'Security', link: '/security/' },
+      { text: 'Product Comparison', link: '/comparison/' },
+      { text: 'Customer Testimonials', link: '/customer/' },
     ]
   },
   {
-    text: 'Order Status',
-    link: '/order/status/'
+    text: 'Partners',
+    // activeMatch: `^/(product)/`,
+    items: [ // TODO: column_submenu
+      { text: 'Scenario', link: '' }, // title
+      { text: 'Brainstorming', link: '/lp/scenario-brainstorming/' },
+      { text: 'Entertainment', link: '/lp/scenario-entertainment/' },
+      { text: 'Meeting', link: '/lp/scenario-meeting/' },
+      { text: 'Presenting', link: '/lp/scenario-presenting/' },
+      { text: 'Remote Collaboration', link: '/lp/scenario-remote/' },
+      { text: 'Training', link: '/lp/scenario-training/' },
+
+      { text: 'Industry', link: '' }, // title
+      { text: 'Distance Learning', link: '/lp/scenario-distance-learning/' },
+      { text: 'Education', link: '/lp/scenario-education/' },
+      { text: 'Engineering', link: '/lp/scenario-engineering/' },
+      { text: 'Marketing', link: '/lp/scenario-marketing/' },
+      { text: 'Professional Services', link: '/lp/scenario-professional-services/' },
+      { text: 'Technology', link: '/lp/scenario-technology/' },
+    ]
+  },
+  {
+    text: 'Resources',
+    // activeMatch: `^/(product)/`,
+    items: [
+      { text: 'Buyer\'s FAQ', link: '/buyer-faq/', nofollow: true },
+      { text: 'Blogs', link: '/blog/', nofollow: true },
+      { text: 'Product Videos', link: '/video-tutorial/', nofollow: true },
+      { text: 'Help Center', link: 'https://knowledge.vibe.us', target: '_blank', nofollow: true },
+      { text: 'Contact Sales', link: '/contact/', nofollow: true },
+      { text: 'Contact Support', link: 'https://knowledge.vibe.us/kb-tickets/new', target: '_blank', nofollow: true },
+      { text: 'Request a Quote', link: '/contact/request-a-quote/', nofollow: true },
+      { text: 'Track Your Order', link: '/order/status/', nofollow: true },
+    ]
+  },
+  {
+    text: 'Partners',
+    link: 'https://vibe.us/reseller/',
+    nofollow: true
   }
 ]
---
+++ src/layout/components/navbar/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>nav</div>
+</template>
+
+<script setup>
+const { frontmatter: fm } = useData();
+</script>
+
+<style lang="sass" scoped>
+
+</style>
